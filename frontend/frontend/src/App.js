d84900732605fd9062ec9ec14df619811e4038d5
slarshhill-code <slarshhill@gmail.com>
2025-08-02 12:20:10 +0200

    Add files via upload

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur AutoTinder Boss 👑</h1>
+      <p>Dashboard opérationnel.</p>
+    </div>
+  );
+}
+
+export default App;
